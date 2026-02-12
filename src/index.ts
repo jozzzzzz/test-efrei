cd5b90e09fdc94ac8107925a251cea6eac3020b3
jojo <jonathan.hayot@efrei.net>
2026-02-12 15:22:32 +0100

    add straight

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,7 @@ export const CardStrength: Record<CardValue, number> = {
   [CardValue.Queen]: 12,
   [CardValue.King]: 13,
   [CardValue.Ace]: 14,
-};
+}
 
 export interface GameResult {
   outcome: FiveCards
@@ -91,37 +91,86 @@ export function HandEvaluator(board: FiveCards, firstHand: Hand, secondHand: Han
 function defineBestHand(cards: Card[]): BestHand | undefined {
   const royalFlush = isRoyalFlushCards(cards);
   if (royalFlush) {
-    return { cards: royalFlush, handStrentgh: HandStrength.RoyalFlush };
+    return { cards: royalFlush, handStrentgh: HandStrength.RoyalFlush }
+  }
+
+  const straightFlush = isStraightFlush(cards);
+  if (straightFlush) {
+    return { cards: straightFlush, handStrentgh: HandStrength.StraightFlush }
   }
 }
 
 function isRoyalFlushCards(cards: Card[]): FiveCards | false {
-  const suits = [Color.Heart, Color.Club, Color.Spade, Color.Diamond];
+  const suits = [Color.Heart, Color.Club, Color.Spade, Color.Diamond]
   const royalValues = ['10', 'J', 'Q', 'K', 'A'];
 
   for (const suit of suits) {
-    const sameColorCards = cards.filter(card => card.color === suit);
+    const sameColorCards = cards.filter(card => card.color === suit)
 
-    if (sameColorCards.length < 5) continue;
+    if (sameColorCards.length < 5) continue
 
-    const royalCards: Card[] = [];
+    const royalCards: Card[] = []
     
     for (const val of royalValues) {
-      const foundCard = sameColorCards.find(c => c.value === val);
+      const foundCard = sameColorCards.find(c => c.value === val)
       if (foundCard) {
-        royalCards.push(foundCard);
+        royalCards.push(foundCard)
       }
     }
 
     if (royalCards.length === 5) {
-      return royalCards as FiveCards;
+      return royalCards as FiveCards
     }
   }
 
-  return false;
+  return false
 }
 
 function isStraightFlush(cards: Card[]): FiveCards | false {
-  const suits = [Color.Heart, Color.Club, Color.Spade, Color.Diamond];
+  const suits = [Color.Heart, Color.Club, Color.Spade, Color.Diamond]
 
+  for (const suit of suits) {
+    const suitCards = cards.filter(c => c.color === suit)
+    if (suitCards.length < 5) continue
+
+    const sorted = suitCards.sort((a, b) => CardStrength[b.value] - CardStrength[a.value])
+
+    for (let i = 0; i <= sorted.length - 5; i++) {
+      const sequence: Card[] = [sorted[i]]
+      
+      for (let j = i + 1; j < sorted.length; j++) {
+        const lastCardValue = CardStrength[sequence[sequence.length - 1].value]
+        const currentCardValue = CardStrength[sorted[j].value]
+
+        if (lastCardValue - currentCardValue === 1) {
+          sequence.push(sorted[j])
+        } else if (lastCardValue === currentCardValue) {
+          continue
+        } else {
+          break
+        }
+
+        if (sequence.length === 5) {
+          return sequence as FiveCards
+        }
+      }
+    }
+
+    // case if straight is A, 2, 3, 4, 5
+    const values = suitCards.map(c => CardStrength[c.value])
+    const aceLowValues = [14, 5, 4, 3, 2]
+    
+    if (aceLowValues.every(v => values.includes(v))) {
+      const wheel = [
+        suitCards.find(c => CardStrength[c.value] === 5)!,
+        suitCards.find(c => CardStrength[c.value] === 4)!,
+        suitCards.find(c => CardStrength[c.value] === 3)!,
+        suitCards.find(c => CardStrength[c.value] === 2)!,
+        suitCards.find(c => CardStrength[c.value] === 14)!,
+      ]
+      return wheel as FiveCards
+    }
+  }
+
+  return false
 }
